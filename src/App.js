afee2aad9f1399f3912224dd676cf627ebb2e7f6
Araf <arafkarim01@gmail.com>
2021-03-14 01:04:04 +0600

    Adding routing with react-router, then creating new routes for our existing homepage and our new hats page. Going over the concept of higher order components through use of the withRouter HOC from react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css"
+import { Switch, Route } from "react-router-dom"
 
 import HomePage from "../src/pages/homepage/homepage.component"
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   )
 }
